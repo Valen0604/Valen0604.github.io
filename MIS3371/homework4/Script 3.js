948ae55c753d691490b4ce5916a95dce123e0bcc
Valen0604 <vjsalerni2004@gmail.com>
2025-04-21 12:39:11 -0500

    Update Script 3.js

--- MIS3371/homework4/Script 3.js
+++ MIS3371/homework4/Script 3.js
@@ -399,15 +399,19 @@ function expireCookie() {
     let IDs = ["firstname", "lastname", "socialsecurity", "zip", "email",
         "userID", "password", "password2", "city", "address1", "phone", "socialsecurity2",
         "socialsecurity3"];
-        let checkbox = document.getElementById("remember");
-    if (checkbox == false) {
+    let checkbox = document.getElementById("remember");
+    if (!checkbox.checked) {
         for (let i = 0; i < IDs.length; i++) {
             document.cookie = IDs[i] + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         }
     } else {
         for (let i = 0; i < IDs.length; i++) {
-            let value = document.getElementById(IDs[i]).value;
-            setCookie(IDs[i], value, 1);
+            if (document.getElementById(IDs[i]).value == "") {
+                continue;
+            } else {
+                let value = document.getElementById(IDs[i]).value;
+                setCookie(IDs[i], value, 1);
+            }
         }
     }
 }

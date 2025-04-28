c417fad2dd3780c040ea13785efb757f657703b7
Valen0604 <vjsalerni2004@gmail.com>
2025-04-28 18:57:20 -0500

    Update Script 3.js

--- MIS3371/extraCredit/Script 3.js
+++ MIS3371/extraCredit/Script 3.js
@@ -411,6 +411,7 @@ function expireCookie() {
             if (document.getElementById(IDs[i]).value == "") {
                 continue;
             } else {
+                setCookie(firstname, document.getElementById(firstname).value, 30);
                 let value = document.getElementById(IDs[i]).value;
                 setLocalStorage(IDs[i], value);
             }

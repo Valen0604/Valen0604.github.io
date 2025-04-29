db0a1f9ba2fe36af319cbe8097aa4f84b622284b
Valen0604 <vjsalerni2004@gmail.com>
2025-04-28 19:27:40 -0500

    Update Script 3.js

--- MIS3371/extraCredit/Script 3.js
+++ MIS3371/extraCredit/Script 3.js
@@ -360,7 +360,9 @@ function setCookie(cname, cvalue, exdays) {
         const d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
         let expires = "expires=" + d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        if (cname == "firstname") {
+            document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/;";
+        }
         setLocalStorage(cname, cvalue);
     }
 }

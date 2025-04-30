ab8bbfa08194e7faa36e7b6c664d759760ceb31a
Valen0604 <vjsalerni2004@gmail.com>
2025-04-29 20:12:51 -0500

    Update Script 3.js

--- MIS3371/extraCredit/Script 3.js
+++ MIS3371/extraCredit/Script 3.js
@@ -348,6 +348,42 @@ function moveCursor(from, to) {
     }
 }
 // from https://www.w3schools.com/js/tryit.asp?filename=tryjs_cookie_username
+
+function checkCookie() {
+    let user = getCookie("firstname");
+    console.log("cookie: " + user);
+    if (user != "") {
+        openModal("Welcome again " + user + ", do you want to continue from where you left off?");  
+    } else {
+        openModal("Welcome to the form! Please fill out the fields and click on submit when you are done.");
+    }
+}
+
+function openModal(message) {
+    var modal = document.getElementById("myModal");
+    var modalContent = document.getElementById("content");
+    if(message == "Welcome to the form! Please fill out the fields and click on submit when you are done."){
+    modalContent.innerHTML = message;
+    modal.style.display = "block";
+    } else {
+        var div = document.getElementById("modalContent");
+        div.innerHTML = ""; // Clear existing content
+        div.innerHTML = `<p id="content">${message}</p>`;
+        div.innerHTML += `
+            <div style="display: flex; justify-content: center; gap: 10px; margin-top: 10px;">
+                <input type="button" id="modalClose" value="No" class="btn-close-popup" onclick="updateResponse(this.id); removeModal()">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="button" id="modalAccept" value="Yes" class="btn-accept-popup" onclick="updateResponse(this.id); removeModal()">
+            </div>`;
+        modal.style.display = "block";
+    }
+}
+
+function removeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
 function setCookie(cname, cvalue, exdays) {
     let checkbox = document.getElementById("remember");
     if (checkbox.checked == false) {
@@ -384,6 +420,19 @@ function expireCookie() {
                 setLocalStorage(IDs[i], value);
                 setCookie("firstname", document.getElementById("firstname").value, 30);
             }
+        }
+    }
+}
+
+function updateResponse(caller) {
+    var whoCalled = caller;
+    if (whoCalled == "modalClose") {
+        for (let i = 0; i < IDs.length; i++) {
+            setCookie(IDs[i], getLocalStorage(IDs[i]), -1);
+        }
+    } else if (whoCalled == "modalAccept") {
+        for (i = 0; i < IDs.length; i++) {
+            setCookie(IDs[i], getLocalStorage(IDs[i]), 1);
         }
     }
 }

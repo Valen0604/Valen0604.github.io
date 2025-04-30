8ff83ae2d8c97afe2a49b4f6dcc08a05b94e6c37
Valen0604 <vjsalerni2004@gmail.com>
2025-04-29 20:18:16 -0500

    Update Script 3.js

--- MIS3371/extraCredit/Script 3.js
+++ MIS3371/extraCredit/Script 3.js
@@ -359,6 +359,22 @@ function checkCookie() {
     }
 }
 
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 function openModal(message) {
     var modal = document.getElementById("myModal");
     var modalContent = document.getElementById("content");
